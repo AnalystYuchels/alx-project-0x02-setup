f2e09fdcece9b7a06449f6b57f8eeeb5ef5a181c
AnalystYuchels <uche4xploit@gmail.com>
2025-07-25 03:50:16 +0100

    Adjusted users.tsx code

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,26 +1,4 @@
-import UserCard from '@/components/common/UserCard';
-import { type UserProps } from '@/interfaces';
-import Header from '@/components/layout/Header';
-
-interface UsersPageProps {
-  users: UserProps[];
-}
-
-const UsersPage = ({ users }: UsersPageProps) => {
-  return (
-    <>
-      <Header />
-      <main className="p-8 space-y-4">
-        <h1 className="text-2xl font-bold mb-4">Users</h1>
-        {users.map((user, index) => (
-          <UserCard key={index} name={user.name} email={user.email} address={user.address} />
-        ))}
-      </main>
-    </>
-  );
-};
-
-export const getStaticProps = async () => {
+export async function getStaticProps() {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
 
@@ -38,6 +16,4 @@ export const getStaticProps = async () => {
       users,
     },
   };
-};
-
-export default UsersPage;
+}
